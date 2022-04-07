8330e67a1ce8700e4937e3f37aa8de9b90362aca
Basil Benedict Victor <basilbennedict@gmail.com>
2022-04-07 14:49:01 +0530

    back to old changes

--- app.py
+++ app.py
@@ -8,74 +8,74 @@ app = Flask(__name__)
 CORS(app)
 
 # Connecting the Database
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://ojxfpuxdeegozu:14972c0cac357bd84101e55870d3a18582d83896bee145e53b97086a01e63e8c@ec2-63-32-248-14.eu-west-1.compute.amazonaws.com:5432/d6p2r68575j5ie'
-# app.config['SQLALCHEMY_TRACK_MODIFICATIONS']= False         
-# db = SQLAlchemy(app)
-# migrate = Migrate(app, db)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://ojxfpuxdeegozu:14972c0cac357bd84101e55870d3a18582d83896bee145e53b97086a01e63e8c@ec2-63-32-248-14.eu-west-1.compute.amazonaws.com:5432/d6p2r68575j5ie'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']= False         
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 
-# # Creating a Model
-# class Todo(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     title = db.Column(db.String(100))
-#     complete = db.Column(db.Boolean, default=False)
-#     date_modified = db.Column(db.DateTime, default=db.func.current_timestamp())
+# Creating a Model
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100))
+    complete = db.Column(db.Boolean, default=False)
+    date_modified = db.Column(db.DateTime, default=db.func.current_timestamp())
 
-# class TodoListSerializer:
-#     def __init__(self, todo_list):
-#         self.todo_list = todo_list
+class TodoListSerializer:
+    def __init__(self, todo_list):
+        self.todo_list = todo_list
 
-#     def is_valid(self):
-#         return self.todo_list is not None
+    def is_valid(self):
+        return self.todo_list is not None
 
-#     def data(self):
-#         if self.is_valid():
-#             result = [
-#                 {
-#                     "id": i.id,
-#                     "title": i.title,
-#                     "completed": i.complete,
-#                     "date_modified": i.date_modified,
-#                 } for i in self.todo_list]
-#             return result
-#         else:
-#             return None
+    def data(self):
+        if self.is_valid():
+            result = [
+                {
+                    "id": i.id,
+                    "title": i.title,
+                    "completed": i.complete,
+                    "date_modified": i.date_modified,
+                } for i in self.todo_list]
+            return result
+        else:
+            return None
 # Routes
 @app.route("/")
 def home():
-    # serialize_instance = TodoListSerializer(Todo.query.all())
-    # if serialize_instance.is_valid():
+    serialize_instance = TodoListSerializer(Todo.query.all())
+    if serialize_instance.is_valid():
         data = {
-            "todo_list": "hello",
+            "todo_list": serialize_instance.data(),
         }
         return jsonify(data), 200
 
 
-# @app.route("/add", methods=["POST"])
-# def add():
-#     title = request.form.get("title")
-#     new_todo = Todo(title=title, complete=False)
-#     db.session.add(new_todo)
-#     db.session.commit()
-#     return {"message":"successfully created"}, 200
+@app.route("/add", methods=["POST"])
+def add():
+    title = request.form.get("title")
+    new_todo = Todo(title=title, complete=False)
+    db.session.add(new_todo)
+    db.session.commit()
+    return {"message":"successfully created"}, 200
 
 
-# @app.route("/update/<int:todo_id>", methods=["PUT"])
-# def update(todo_id):
-#     todo = Todo.query.filter_by(id=todo_id).first()
-#     todo.complete = not todo.complete
-#     db.session.commit()
-#     return {"message":"successfully completed"}, 200
+@app.route("/update/<int:todo_id>", methods=["PUT"])
+def update(todo_id):
+    todo = Todo.query.filter_by(id=todo_id).first()
+    todo.complete = not todo.complete
+    db.session.commit()
+    return {"message":"successfully completed"}, 200
 
 
-# @app.route("/delete/<int:todo_id>", methods=["DELETE"])
-# def delete(todo_id):
-#     todo = Todo.query.filter_by(id=todo_id).first()
-#     db.session.delete(todo)
-#     db.session.commit()
-#     return {"message":"successfully deleted"}, 200
+@app.route("/delete/<int:todo_id>", methods=["DELETE"])
+def delete(todo_id):
+    todo = Todo.query.filter_by(id=todo_id).first()
+    db.session.delete(todo)
+    db.session.commit()
+    return {"message":"successfully deleted"}, 200
 
 
 if __name__ == "__main__":
-    # db.create_all()
+    db.create_all()
     app.run(debug=True)
